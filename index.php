8cb9e5d30166b52f4b709fe1b9c9195169c7cecb
mohammadkamil <mohammadkamil1991@gmail.com>
2022-10-21 20:17:44 +0800

    done

---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+require_once(__DIR__ . '/vendor/autoload.php');
+
+# Create a new Xls Reader
+$reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
+
+// Tell the reader to only read the data. Ignore formatting etc.
+$reader->setReadDataOnly(true);
+
+// Read the spreadsheet file.
+$spreadsheet = $reader->load(__DIR__ . '/VehicleRouteMovement.xlsx');
+
+$vehicleroutemovement = $spreadsheet->getSheet(2);
+$vehiclecost = $spreadsheet->getSheet(3);
+
+$vehiclemovementdata = $vehicleroutemovement->rangeToArray('A2:G98');
+$vehiclecostdata = $vehiclecost->rangeToArray('A2:C16');
+$arraymaintenancebybranch=[];
+$arraybranch=[];
+// $taken = array();
+
+foreach($vehiclemovementdata as $key => $item) {
+   
+    if(!in_array($item[6], $arraybranch)&&$item[6]!="") {
+        $arraybranch[] = $item[6];
+    } 
+}
+// echo json_encode($arraybranch);
+
+foreach($arraybranch as $branch){
+    $currentbranch=$branch;
+    $totalmaintenancecost=0;
+    $totalmaintenancebycar=0;
+    foreach($vehiclecostdata as $vehicle){
+        $currentcar=$vehicle[0];
+        // echo $currentcar;
+        $totalcostcar=0;
+        $totalmilage=0;
+        foreach($vehiclemovementdata as $currentmovement){
+            if($currentmovement[2]==$currentcar&&$currentmovement[6]==$currentbranch){
+                $totalmilage=$currentmovement[4]+$totalmilage;
+            }
+        }
+        // echo $totalmilage;
+        $totalmaintenancebycar=$totalmilage/$vehicle[1]*$vehicle[2];
+        // echo $totalmaintenancecost. ' current car ' . $currentcar;
+        $totalmaintenancecost=$totalmaintenancecost+$totalmaintenancebycar;
+    }
+
+    $arraymaintenancebybranch[$currentbranch]=$totalmaintenancecost;
+}
+echo json_encode($arraymaintenancebybranch);
+
+// output the data to the console, so you can see what there is.
+// // echo json_encode($vehiclemovementdata);
+// die(print_r($vehiclemovementdata, true));
+?>
